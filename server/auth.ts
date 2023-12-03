1d13c676c9d5a298fd726961857e5df2734a8195
Felix Kaasa <mrKaasa@protonmail.com>
2023-12-03 20:17:57 +0100

    added supabase adapter

--- server/auth.ts
+++ server/auth.ts
@@ -1,6 +1,7 @@
 import { DefaultSession, NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import DiscordProvider from "next-auth/providers/discord";
+import { SupabaseAdapter } from "@auth/supabase-adapter";
 
 declare module "next-auth" {
   interface Session extends DefaultSession {
@@ -21,6 +22,10 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
     }),
   ],
+  adapter: SupabaseAdapter({
+    url: process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+    secret: process.env.NEXT_PUBLIC_SUPABASE_KEY as string,
+  }),
   cookies: {
     sessionToken: {
       name: `next-auth.session-token`,
